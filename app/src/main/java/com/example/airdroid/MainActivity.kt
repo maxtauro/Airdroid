dd3442ddc07cbaf6798864a783c3ab0b71148e99
Max Tauro <maxtauro@hotmail.com>
2019-06-24 20:23:40 -0400

    added pngs for devices

--- app/src/main/java/com/example/airdroid/MainActivity.kt
+++ app/src/main/java/com/example/airdroid/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.airdroid
 
 import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
+
+    private val REQUEST_ENABLE_BT = 1000 //TODO move this somewhere else
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
-        if (bluetoothAdapter == null) {
-            // Device doesn't support Bluetooth
-        }
+    override fun onResume() {
+        super.onResume()
+
+//        val bluetoothAdapter: BluetoothAdapter =
+//            BluetoothAdapter.getDefaultAdapter() ?: TODO("Handle device not having bluetooth compatibility")
+//
+//        if (!bluetoothAdapter.isEnabled) {
+//            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+//            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+//        }
     }
 
     fun closeWindow(view: View) {
