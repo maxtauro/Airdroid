17a4ba13c3ba4781d315e3acdbe0b92031c012ba
maxwelltauro <maxwell.tauro@toasttab.com>
2019-06-24 23:03:52 -0400

    some barebones layout and boilerplate code

--- app/src/main/java/com/example/airdroid/MainActivity.kt
+++ app/src/main/java/com/example/airdroid/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.airdroid
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -19,13 +20,30 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-//        val bluetoothAdapter: BluetoothAdapter =
-//            BluetoothAdapter.getDefaultAdapter() ?: TODO("Handle device not having bluetooth compatibility")
-//
-//        if (!bluetoothAdapter.isEnabled) {
-//            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-//            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-//        }
+        val bluetoothAdapter: BluetoothAdapter =
+            BluetoothAdapter.getDefaultAdapter() ?: TODO("Handle device not having bluetooth compatibility")
+
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+
+        val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter.bondedDevices
+
+
+        if (!areAirpodsPaired(pairedDevices)) {
+            TODO("No airpods paired, prompt user for pairing workflow")
+        }
+
+
+
+        val stop = "hammer time"
+    }
+
+
+    private fun areAirpodsPaired(pairedDevices: Set<BluetoothDevice>?): Boolean {
+        // TODO find cleaner way to check if a device is airpod
+        return pairedDevices?.any { device -> device.name.equals("Airpods", true) } ?: return false
     }
 
     fun closeWindow(view: View) {
