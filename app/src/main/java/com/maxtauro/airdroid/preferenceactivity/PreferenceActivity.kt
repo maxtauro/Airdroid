4b93db1c5228900e46a0c5f101e1c2c833dbc857
Max Tauro <maxtauro@hotmail.com>
2019-12-22 20:43:43 -0500

    Migrated to AndroidX, did barebones set up of preference activities

--- app/src/main/java/com/maxtauro/airdroid/preferenceactivity/PreferenceActivity.kt
+++ app/src/main/java/com/maxtauro/airdroid/preferenceactivity/PreferenceActivity.kt
@@ -2,6 +2,7 @@ package com.maxtauro.airdroid.preferenceactivity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.preference.PreferenceFragmentCompat
 import com.maxtauro.airdroid.R
 
@@ -9,12 +10,20 @@ class PreferenceActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings_activity)
+        setContentView(R.layout.activity_preferences)
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.settings, SettingsFragment())
             .commit()
+
+        val toolbar = findViewById<Toolbar>(R.id.preference_activity_toolbar)
+        setSupportActionBar(findViewById(R.id.preference_activity_toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
